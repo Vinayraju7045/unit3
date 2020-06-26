ada47ccf35b12b7164f21874e1ee2ec84e8f8053
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-26 09:48:20 +0530

    Add files via upload

---
+++ unit3_ass2_avg_10num.py
@@ -0,0 +1,76 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "1\n",
+      "average is 0.1\n",
+      "2\n",
+      "average is 0.3\n",
+      "3\n",
+      "average is 0.6\n",
+      "4\n",
+      "average is 1.0\n",
+      "5\n",
+      "average is 1.5\n",
+      "6\n",
+      "average is 2.1\n",
+      "7\n",
+      "average is 2.8\n",
+      "8\n",
+      "average is 3.6\n",
+      "9\n",
+      "average is 4.5\n",
+      "0\n",
+      "average is 4.5\n"
+     ]
+    }
+   ],
+   "source": [
+    "sum=0\n",
+    "n=10\n",
+    "for i in range(0,n):\n",
+    "    num=int(input())\n",
+    "    sum=sum+num\n",
+    "    i+=1\n",
+    "    print ('average is',sum/10)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
