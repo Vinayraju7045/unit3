14ec92152caacdf4f81c2d93636e1089c75e0dd2
Vinayraju7045 <67084004+Vinayraju7045@users.noreply.github.com>
2020-06-26 09:57:48 +0530

    Add files via upload

---
+++ unit3_ass2_character_count.py
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Count of all characters in string is :\n",
+      " Counter({'s': 2, 'p': 1, 'y': 1, 't': 1, 'h': 1, 'o': 1, 'n': 1, 'c': 1, 'l': 1, 'a': 1})\n"
+     ]
+    }
+   ],
+   "source": [
+    "from collections import Counter \n",
+    "test_str = \"pythonclass\"\n",
+    "res = Counter(test_str) \n",
+    "print (\"Count of all characters in string is :\\n \"+  str(res))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
